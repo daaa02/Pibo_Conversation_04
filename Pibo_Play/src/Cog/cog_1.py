8860d30bc14a381372fe1c0afdf7f67ec6967263
daaa02 <dyk98498@gmail.com>
2023-07-26 22:05:27 +0900

    Update_230726

--- Pibo_Play/src/Cog/cog_1.py
+++ Pibo_Play/src/Cog/cog_1.py
@@ -212,11 +212,12 @@ class Cog():
         cwc.writerow(['%Rejections', ])
         cwc.writerow(['%Misrecognitions', ])
 
-        # 5. 활동 완료 기록
-        today_end = datetime.now().strftime('%m%d_%H%M')        
-        gss.write_sheet(name=self.user_name, today=f'end_{today_end}', activities=filename)
-        
-        
+        try:
+            # 5. 활동 완료 기록
+            today_end = datetime.now().strftime('%m%d_%H%M')        
+            gss.write_sheet(name=self.user_name, today=f'end_{today_end}', activities=filename)
+        except Exception as e:
+            pass
         
         
 if __name__ == "__main__":
