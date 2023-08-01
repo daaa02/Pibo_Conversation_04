cece90e76a6c619bad1209197104f826a64b675b
daaa02 <dyk98498@gmail.com>
2023-08-01 18:23:13 +0900

    Update_230801

--- Pibo_Play/src/Cog/cog_1.py
+++ Pibo_Play/src/Cog/cog_1.py
@@ -213,9 +213,8 @@ class Cog():
         cwc.writerow(['%Misrecognitions', ])
 
         try:
-            # 5. 활동 완료 기록
-            today_end = datetime.now().strftime('%m%d_%H%M')        
-            gss.write_sheet(name=self.user_name, today=f'end_{today_end}', activities=filename)
+            # 5. 활동 완료 기록    
+            gss.write_sheet(name=self.user_name, today=f'(4)_{today}', activities=filename)
         except Exception as e:
             pass
         
