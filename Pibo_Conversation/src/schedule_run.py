6667d6110f29e5bd067e7535551a2e00b507e04e
daaa02 <dyk98498@gmail.com>
2023-08-02 15:00:44 +0900

    Update_230802

--- Pibo_Conversation/src/schedule_run.py
+++ Pibo_Conversation/src/schedule_run.py
@@ -48,32 +48,35 @@ class RunSchedule():
         # print(data2)                                  # [점수], [점수], ...
         self.completion = len(data2)
         print('완료한 활동 개수:', self.completion)
-
-        # 고정된 스케줄로 진행(~3일차)
         
+
+        ## 고정된 스케줄로 진행(~3일차)
+                
+        # 최초 실행시 자기소개 시나리오 실행
         if self.completion == 0:
             self.act = f'Pibo_Conversation/src/greeting.py'
-            
+        
         if 1 <= self.completion <= 8:
-            # 완료한 활동 개수가 짝수면 놀이, 홀수면 대화           
             fix = {1:'Pibo_Conversation/src/Fairytale/19_shepherd.py', 2:'Pibo_Conversation/src/Roleplay/02_strong.py',
                    3:'Pibo_Conversation/src/Solution/01_badword.py', 4:'Pibo_Play/src/Com/com_4.py', 5:'Pibo_Play/src/Soc/soc_6.py',
                    6:'Pibo_Conversation/src/Etiquette/03_cough.py', 7:'Pibo_Play/src/Cog/cog_1.py', 8:'Pibo_Play/src/Mus/mus_11.py'}   
             self.act = fix.get(self.completion)
+            
         
-        # 선호도 계산해서 활동 스케줄 결정(4일차~)
+        ## 선호도 계산해서 활동 스케줄 결정(4일차~)
+        
         if 9 <= self.completion <= 10 or self.completion >= 12:
-            for i in range(0, len(data2)):              # 점수 값이 string 형태로 들어있어서 flaot로 변환
+            for i in range(0, len(data2)):
                 for j in range(0, 4):
-                    data2[i][j] = float(data2[i][j])
+                    data2[i][j] = float(data2[i][j])    # 점수 값이 string 형태로 들어있어서 flaot로 변환
             print(data2)
 
             for i in range(0, len(data2)-1):            # 각 항목끼리 모두 합연산
                 data3 = [sum(i) for i in zip(*data2)]   # data1 목록 만큼
             print("선호도 총합:", data3)   
             
-            result = us.update(new_preference=data3)[0]
-            
+            result = us.update(new_preference=data3)[0]    
+                    
             
             if result == '사회성':            
                 # 완료한 활동 개수가 짝수면 놀이, 홀수면 대화
@@ -116,11 +119,9 @@ class RunSchedule():
                     rand = random.choice(['03_tiny', '04_flying'])
                     self.act = f'Pibo_Conversation/src/Roleplay/{rand}.py'
         
-        # 마지막 활동은 헤어짐 시나리오: 
+        # 마지막 활동은 헤어짐 시나리오 실행 
         if self.completion == 11:
             self.act = f'Pibo_Conversation/src/goodbye.py'           
-        
-        # out = subprocess.check_output([f'python3 /home/pi/Pibo_Package_04/Pibo_Conversation/src/greeting.py'], shell=True, stderr=subprocess.STDOUT, encoding="utf-8")  
         
         try:
             folder = "/home/pi/UserData"
@@ -147,7 +148,7 @@ class RunSchedule():
                 text_to_speech(text="파이보랑 또 놀자!")            
                 motion.set_motion("m_wakeup", 1)
                 subprocess.run(['python3 /home/pi/Pibo_Package_04/Pibo_Conversation/src/start_touch.py'], shell=True)
-            # 여기까지 주석 ㄱㄱ
+            # 여기까지 주석!! 대신 다음 활동 하려면 매번 재부팅 해야함
             
         except Exception as ex:
             with open('/home/pi/pibo_errmsg', 'w') as f:
